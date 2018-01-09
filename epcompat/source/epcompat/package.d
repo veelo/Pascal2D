c72ba361985e44aaeee4a831c840d5411311bbb4
Bastiaan Veelo <Bastiaan@SARC.nl>
2018-01-09 12:54:07 +0100

    Document epcompat.

--- epcompat/source/epcompat/package.d
+++ epcompat/source/epcompat/package.d
@@ -1,3 +1,36 @@
+/**
+Provides type compatibility with, and comparable features to Extended Pascal.
+
+This is the library that translated Pascal sources rely on. It also provides
+features that can be of value in hand-written D code. The epcompat library is
+supplied as a dub subpackage so it supports that use case.
+
+$(TABLE,
+    $(TR $(TH Module) $(TH Purpose))
+
+    $(TR $(TD array) $TD(
+        Fixed length array types that can start at any index.
+    ))
+    $(TR $(TD enumeration) $(TD
+        Brings the members of an enumeration into scope, as if the following
+        code would be in a `with` block.
+    ))
+    $(TR $(TD initial) $(TD
+        Changes `.init` to a custom default value.
+    ))
+    $(TR $(TD ordinal) $(TD
+        An integral type with specific inclusive bounds.
+    ))
+    $(TR $(TD set) $(TD
+        Sets of integral values, ordinal values or members of enumerations.
+    ))
+    $(TR $(TD string) $(TD
+        String types that are compatible with native D `string`, and binary
+        compatible with Prospero Extended Pascal strings in file i/o. 
+    ))
+)
+ */
+
 module epcompat;
 
 public import epcompat.array;
