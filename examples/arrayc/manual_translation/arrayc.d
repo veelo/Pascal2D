fb7e4ca77ef894a15b1ab28d1f5fa249b26fa9d4
Bastiaan Veelo <Bastiaan@SARC.nl>
2018-01-10 19:51:21 +0100

    Typo

--- examples/arrayc/manual_translation/arrayc.d
+++ examples/arrayc/manual_translation/arrayc.d
@@ -27,10 +27,10 @@ END.
 import std.stdio;
 import epcompat;
 
-enum days {sun, man, tues, weds, thurs, fri, sat};
+enum days {sun, mon, tues, weds, thurs, fri, sat};
 mixin withEnum!days;
 
-alias dname = string; // Variable length instead of fixed.
+@EPString(8) alias dname = string; // Variable length instead of fixed.
 
 days d;
 
@@ -41,7 +41,7 @@ dname DayName(days fd)
     /*alias abbrevs = char
     [5 - 1][days];*/
     alias abbrevs = string[days];
-    immutable abbrevs DayNames = [sun: "Sun", man: "Man", tues: "Tues",
+    immutable abbrevs DayNames = [sun: "Sun", mon: "Mon", tues: "Tues",
                                   weds: "Weds", thurs: "Thurs", fri: "Fri",
                                   sat: "Satur"];
     return DayNames[fd] ~ "day";
@@ -49,8 +49,8 @@ dname DayName(days fd)
 
 void main()
 {
-    /*foreach (d; days.fri..days.man)
-        writeln(DayName(d));*/
-    for (d = days.fri; d >= days.man; d--)
+    //foreach_reverse (d; mon..sat)
+    //    writeln(DayName(d));
+    for (d = fri; d >= mon; d--)
         writeln(DayName(d));
 }
