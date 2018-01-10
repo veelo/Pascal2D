89548e7b38e1afe397adc1b5e09291095dcbd688
Bastiaan Veelo <Bastiaan@SARC.nl>
2018-01-10 13:38:55 +0100

    Unit tests are run without arguments, don't error.

--- source/main.d
+++ source/main.d
@@ -3,6 +3,11 @@ import std.file;
 import epparser;
 import p2d;
 
+version(unittest) {
+    // Unit tests are run without arguments, don't error.
+    void main(){}; 
+} else
+
 int main(string[] args)
 {
     if (args.length != 2)
