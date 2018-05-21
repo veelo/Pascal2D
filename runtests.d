094baee72a1b86db22ef4d4a08271b36135a2f5d
Bastiaan Veelo <Bastiaan@SARC.nl>
2018-05-21 21:43:07 +0200

    Assert correctness of arraybase example.

--- runtests.d
+++ runtests.d
@@ -102,6 +102,11 @@ int main()
         } else {
             remove(outlog);
         }
+        enum arraydat = "examples\\arraybase\\array.dat";
+        import std.file;
+        assert(getSize(arraydat) == 76);
+        assert(cmp(cast(int[])read(arraydat),
+                   [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]) == 0);
     }
     writeln("=== runtests: completed example \"arraybase\"");
 
